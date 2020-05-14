a172801566e530c6366f87e8e89bbd7816d30883
Ross MacDonald <rmac2289@gmail.com>
2020-05-14 01:37:42 -0700

    minor updates

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBullhorn } from '@fortawesome/free-solid-svg-icons'
+import TokenService from '../../services/token-service'
 
 export default function Nav(props){
+    const [loggedIn, setLoggedIn] = useState(false)
+    function handleLogoutClick(){
+    }
+
+    function renderLogoutLink(){
+        return (
+            <Link
+              className="navLink"
+              onClick={handleLogoutClick}
+              to='/'>
+              Logout
+            </Link>
+        )
+      }
+      function isLoggedIn(){
+        if (TokenService.hasAuthToken()){
+            setLoggedIn(true)
+        }
+        setLoggedIn(false)
+    }
+    
+    
+    function renderLoginLink() {
+        return (
+            <>
+            <li><Link className="navLink" to='/login' onClick={props.login}>Login</Link></li>
+            <li><Link className="navLink" to='/signuppage'>Register</Link></li>
+            </>
+        )
+      }
     return (
         <nav>
             <h5 id="linkLogo"><Link id="navLogo" to="/">hearsay <span><FontAwesomeIcon id="bullhorn" icon={faBullhorn}/></span></Link></h5>
-            <ul>
-                <li><Link className="navLink" to='/login' onClick={props.login}>Login</Link></li>
-                <li><Link className="navLink" to='/signuppage'>Register</Link></li>
+            <ul>{isLoggedIn ? renderLogoutLink():renderLoginLink()}
                 <li><Link className="navLink" to='/'>Home</Link></li>
                 <li><Link className="navLink" to='/departments'>Write Review</Link></li>
                 <li><Link className="navLink" to='/forum'>Forum</Link></li>
