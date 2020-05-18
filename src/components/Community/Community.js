f04e1673e3545566a9ed3487e221dcfe4090ccaa
Ross MacDonald <rmac2289@gmail.com>
2020-05-18 12:47:48 -0700

    minor text update

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -1,44 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Community.css'
-import Agencies from '../../Agencies'
 import { Link } from 'react-router-dom'
 import { Button } from '../../Utils/Utils'
 
 export default function Community(props){
-    const [setSelect, setSelectState] = useState("")
-    const [setDeptSelect, setDeptSelectState] = useState("")
-
-    function selectChange(event){
-        setSelectState(event.target.value)
-    }
-    function deptSelectChange(event){
-        setDeptSelectState(event.target.value)
-    }
-    const states = Object.keys(Agencies.States)
-    const stateList = states.map((value, index) => {
-        return <option key={index} value={value}>{value}</option>    
-    })
-    const depts = Agencies.States[setSelect]
     return (
         <section>
                 <header>
                     <h2>see what others are saying in your community</h2>
                 </header>
-                <p>Find out what others in the country are saying about their local officials. Search for your city to see reviews about your local law enforcement.
-                    Pick your state, then choose the department from the list.
+                <p>Find out what others in the country are saying about their local officials. Clik the link below to search by department to see reviews for any law enforcement in the country.
                 </p>
                 <div className="citySearch">
-                <select className="select" onChange={selectChange}>
-                    <option defaultValue="find your state">find your state</option>
-                    {stateList} 
-                </select>
-                <select onChange={deptSelectChange} className="select">
-                    {setSelect !== "" && <option defaultValue="choose department">choose department</option>}
-                    {setSelect !== "" && depts.sort().map((value, index) => {
-                    return <option value={value} key={index}>{value}</option>})} 
-                </select>
                 <div>
-                    <Button><Link id="showMe" to="/singlereview">show me reviews</Link></Button>
+                    <Button><Link id="showMe" to="/departments">show me reviews</Link></Button>
                 </div>
                 </div>
         </section>
