e54887110ee0e47efc5b138cdb9b5c441c080bb3
Ross MacDonald <rmac2289@gmail.com>
2020-05-28 15:48:55 -0700

    added comments

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -11,9 +11,18 @@ export function Textarea({ className, ...props }) {
     <textarea className={['Textarea', className].join(' ')} {...props} />
   )
 }
+
 export function Input({ className, ...props }) {
   return (
     <input className={['Input', className].join(' ')} {...props} />
+  )
+}
+
+export function Required({ className, ...props }) {
+  return (
+    <span className={['Required', className].join(' ')} {...props}>
+      &#42;
+    </span>
   )
 }
 
