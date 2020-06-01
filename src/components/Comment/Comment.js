e49108366b4e36ae47152f90eddc54d6585a6762
Ross MacDonald <rmac2289@gmail.com>
2020-06-01 10:17:14 -0700

    removed like dislike buttons for now

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
 import './Comment.css';
 
 export default function Post(props){
-    const [count, setCount] = useState(0);
-    const [dislikeCount, setDislikeCount] = useState(0);
-    
-    const addLike = () => {
-        setCount(count + 1 );
-      };
-    const dislike = () => {
-      setDislikeCount(dislikeCount + 1);
-    };
       
     return (
 <li className="postListItem">
         <p className="postContent">{props.text}</p>
-          <div className="like" >
-            <div onClick={addLike}>
-              <FontAwesomeIcon id="icon" icon={faHeart} />{count}
-            </div>
-            <div onClick={dislike}>
-            <FontAwesomeIcon id="icon" icon={faThumbsDown} />{dislikeCount}
-            </div>
-          </div>
           <div><strong>posted by:</strong> {props.user}</div>
           <div><strong>on:</strong> {props.date}</div>
       </li>
