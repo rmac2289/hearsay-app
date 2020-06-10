e6a89a8e6ea5101512db5c5798c10858f5e2f089
Ross MacDonald <rmac2289@gmail.com>
2020-06-10 13:55:54 -0700

    added like and dislike button function

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  // API_ENDPOINT: 'https://fierce-cliffs-47097.herokuapp.com/api',
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: 'https://fierce-cliffs-47097.herokuapp.com/api',
   TOKEN_KEY: "hearsay-app-auth-token"
 };
